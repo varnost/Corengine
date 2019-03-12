9e27bd1aed26d1d73ff9ea92d499e4c3c2363d26
tyler <tylerhoyt93@gmail.com>
2019-03-11 21:55:47 -0700

    Now using event time

--- src/main/java/io/varnost/base/LogStream.java
+++ src/main/java/io/varnost/base/LogStream.java
@@ -1,12 +1,17 @@
 package io.varnost.base;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
@@ -21,15 +26,31 @@ public class LogStream {
   public LogStream() {
     // Setup up execution environment
     see = StreamExecutionEnvironment.getExecutionEnvironment();
+    see.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
   }
-
-  public DataStream<ObjectNode> openStream() {
+  public DataStream<ObjectNode> openStream() {return openStream(Time.seconds(10));}
+  public DataStream<ObjectNode> openStream(Time allowedLateness) {
     Properties properties = new Properties();
     properties.setProperty("bootstrap.servers", "kf-service:9092");
     properties.setProperty("group.id", "varnost-content");
-    return  see.addSource(new FlinkKafkaConsumer011<>("log-input",
-            new JSONKeyValueDeserializationSchema(false), properties).setStartFromLatest())
-            .map((MapFunction<ObjectNode, ObjectNode>) jsonNodes -> (ObjectNode) jsonNodes.get("value"));
+
+    SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    DataStream<ObjectNode> stream = see.addSource(new FlinkKafkaConsumer011<>("log-input",
+            new JSONKeyValueDeserializationSchema(false), properties).setStartFromLatest());
+    return stream
+        .map((MapFunction<ObjectNode, ObjectNode>) jsonNodes -> (ObjectNode) jsonNodes.get("value"))
+        .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<ObjectNode>(allowedLateness) {
+          @Override
+          public long extractTimestamp(ObjectNode node) {
+            try {
+              return f.parse(node.get("@timestamp").asText()).getTime();
+            } catch (ParseException e) {
+              e.printStackTrace();
+              return 0;
+            }
+          }
+        });
   }
 
   public JobExecutionResult closeStream(DataStream<Alert> alerts) throws Exception {
